f3390aa57959b99bd77cf3fee3566f4ec9379e86
Kranig, Tyler L <Tyler.Kranig@tdameritrade.com>
2019-06-24 16:58:32 -0500

    finished styling for pokedex

--- reactLearning/udemyFull/pokedex/pokedex/src/App.js
+++ reactLearning/udemyFull/pokedex/pokedex/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Pokedex from './Component/Pokedex.js';
+import Pokegame from './Component/Pokegame.js';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <Pokedex />
+      <Pokegame />
     </div>
   );
 }
