c66bb6f4c3deb5fbe01a73392cebecd4bd21bc55
Kranig, Tyler L <Tyler.Kranig@tdameritrade.com>
2019-06-24 15:11:47 -0500

    Finished the initial pokedex

--- reactLearning/udemyFull/pokedex/pokedex/src/Component/Pokecard.js
+++ reactLearning/udemyFull/pokedex/pokedex/src/Component/Pokecard.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import '../Style/Pokecard.css';
+const url = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+let padToThree = (number) => ( number <= 999 ? `00${number}`.slice(-3) : number )
 
 class Pokecard extends React.Component {
     render() {
+        const { id, name, type, exp } = this.props;
+        let imgSrc = `${url}${padToThree(id)}.png`;
         return (
-            <div>
-                Test
+            <div className='Pokecard'>
+               <h1 className="Pokecard-title">{name}</h1>
+               <img src={imgSrc} />
+               <div className="Pokecard-data">Type: {type}</div>
+               <div className="Pokecard-data">EXP: {exp}</div>
             </div>
         );
     }
