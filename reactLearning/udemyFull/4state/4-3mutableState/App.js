41a6d86ede86c5bb9eaa8cdc064694f0aa053a7a
tyler <tylerkranig@gmail.com>
2019-07-08 08:36:23 -0500

    started more state

---
+++ reactLearning/udemyFull/4state/4-3mutableState/App.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <IconList />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ reactLearning/udemyFull/4state/4-3mutableState/IconList.js
@@ -0,0 +1,46 @@
+class IconList extends React.Component {
+  static defaultProps = {
+    options: [
+      { id: 1, name: "angry" },
+      { id: 2, name: "archive" },
+      { id: 3, name: "at" },
+      { id: 4, name: "archway" },
+      { id: 5, name: "baby" },
+      { id: 6, name: "bell" },
+      { id: 7, name: "bolt" },
+      { id: 8, name: "bone" },
+      { id: 9, name: "car" },
+      { id: 10, name: "city" },
+      { id: 11, name: "cloud" },
+      { id: 12, name: "couch" }
+    ]
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      icons: []
+    };
+  }
+
+  addIcon = () => {
+    let idx = Math.floor(Math.random() * this.props.options.length);
+    let newIcon = this.props.options[idx];
+    //Wrong way
+    //   let temp = this.state.icons;
+    //   temp.push(newIcon);
+    //   this.setState({ icons: temp })
+
+    this.setState({ icons: [...this.state.icons, newIcon] });
+  };
+
+  render() {
+    const icons = this.state.icons.map(i => <i className={`fas fa-${i}`} />);
+    return (
+      <div>
+        <h1>Icons: {icons}</h1>
+        <button onClick={this.addIcon}>Add New Icon</button>
+      </div>
+    );
+  }
+}
