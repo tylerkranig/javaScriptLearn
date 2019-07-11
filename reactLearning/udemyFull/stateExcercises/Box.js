85c2ee191c1b1f1dd36d0cd405cbfdcca8bd8ce7
tkranig <tkranig@iastate.edu>
2019-07-11 08:03:39 -0500

    finished state excercises

---
+++ reactLearning/udemyFull/stateExcercises/Box.js
@@ -0,0 +1,40 @@
+function choice(arr) {
+  let randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
+
+class Box extends React.Component {
+  static defaultProps = {
+    allColors: ["purple", "magenta", "blue", "green"]
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: choice(this.props.allColors),
+    }
+  }
+
+  pickColor() {
+    let newColor;
+
+    do {
+      newColor = choice(this.props.allColors);
+    } while(newColor === this.state.color)
+
+    this.setState({color: newColor});
+  }
+
+  handleClick = () => {
+    this.pickColor();
+  }
+
+  render() {
+    return (
+      <div className="Box" style={{backgroundColor: this.state.color}} onClick={this.handleClick}>
+        
+      </div>
+    )
+  }
+
+}
