40abee341304e56c0c12cd8d74a7a23978b7704e
tkranig <tkranig@iastate.edu>
2019-07-15 08:24:25 -0500

    multi input forms

--- reactLearning/udemyFull/6forms/Form.js
+++ reactLearning/udemyFull/6forms/Form.js
@@ -2,7 +2,9 @@ class Form extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            fullName: ""
+            fullName: "",
+            email: "",
+            password: ""
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -15,8 +17,8 @@ class Form extends React.Component {
 
     handleChange(e) {
         this.setState({
-            fullName: e.target.value
-        })
+            [e.target.name]: e.target.value
+        });
     }
 
     render() {
@@ -24,10 +26,22 @@ class Form extends React.Component {
             <form onSubmit={this.handleSubmit}>
                 <label>Fullname: </label>
                 <input 
-                    name="fullname" 
+                    name="fullName" 
                     value={this.state.fullName} 
                     onChange={this.handleChange}
                 />
+                <input 
+                    type='email' 
+                    name="email"
+                    placeholder='email' 
+                    value={this.state.email}
+                    onChange={this.handleChange} />
+                <input 
+                    type='password' 
+                    name="password"
+                    placeholder='password'
+                    value={this.state.password} 
+                    onChange={this.handleChange}/>
                 <button>Add</button>
             </form>
         );
